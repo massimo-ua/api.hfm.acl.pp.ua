cdab078fc8ae6c36a14dd0df661e0096e1b011ba
Maxim Grishkov <grishkov.m@gmail.com>
2017-12-25 00:59:54 +0200

    Use the PORT environment variable to set the port, make it required; Make the tests pass (npm run test-web)

--- config/index.js
+++ config/index.js
@@ -1,5 +1,15 @@
-module.exports = {
+'use strict';
+const joi = require('joi');
+const envVarsSchema = joi.object({
+    APP_PORT: joi.number().integer().min(2000).max(10000).required()
+}).unknown().required();
+const { error, value: envVars } = joi.validate(process.env, envVarsSchema)
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`)
+};
+const config = {
     app: {
-        port: process.env.APP_PORT || 3000
+        port: envVars.APP_PORT
     }
-}
+};
+module.exports = config;
