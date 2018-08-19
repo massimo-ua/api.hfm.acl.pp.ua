ae2dee15bcf83698985f2757981db81512176834
Maxim Gryshkov <grishkov.m@gmail.com>
2018-08-19 23:01:03 +0300

    transaction block completed

--- modules/transaction/routes/getAll.js
+++ modules/transaction/routes/getAll.js
@@ -1,7 +1,36 @@
 'use strict';
+const compose = require('koa-compose'),
+    middleware = require('../../../middleware'),
+    { allQuery: querySchema } = require('../validators'),
+    {transaction: Transaction} = require('../models'),
+    sequelize = require('sequelize'),
+    User = require('../../user/model'),
+    { throwError, to } = require('../../../helpers');
 
-function getAll(ctx, next) {
-
+async function getAll(ctx) {
+    let err, transactions;
+    const { year } = ctx.query;
+    [err, transactions] = await to(Transaction.findAll({ 
+        where: sequelize.where(
+            sequelize.literal('extract(YEAR FROM transaction_date)'),
+            year
+        ), 
+        include: [
+            'account',
+            { model: User, as: 'user', attributes: ['_id','login','name']},
+            'params',
+            'source',
+            'target'
+        ] }));
+    if(err || !transactions) {
+        throwError('No transactions found', true, 404);
+    }
+    ctx.body = transactions;
 }
 
-module.exports = getAll;
+module.exports = compose([
+    middleware.validator({
+        query: querySchema
+    }),
+    getAll
+]);
--- modules/transaction/validators/index.js
+++ modules/transaction/validators/index.js
@@ -4,4 +4,5 @@ module.exports = {
     update: require('./update'),
     byId: require('./byId'),
     addParam: require('./addParam'),
+    allQuery: require('./allQuery'),
 };
